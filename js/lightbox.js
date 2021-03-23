de483207662c108b50c1254f4c5b4b819925066c
Ivan Torres <tec.adriantf@gmail.com>
2021-03-23 11:11:04 -0700

    update click imagen

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,25 +1,27 @@
-const imagenes = document.querySelector('.img-galeria')
+const imagenes = document.querySelectorAll('.img-galeria')
 const imagenesLight = document.querySelector('.agregar-imagen')
 const contenedorLight = document.querySelector('.imagen-light')
-const hamburguer1 = document.querySelector('.hamburguer');
+const hamburguer1 = document.querySelector('.hamburguer'); 
+
 
 imagenes.forEach(imagen =>{
-    imagenes.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAttribute('src'))
-})
+    imagen.addEventListener('click', ()=>{
+       aparecerImagen(imagen.getAttribute('src'))
+     // alert("funciona")
+    })
 })
 
-contenedorLight.addEventListener('click', (e)=>{
-    if(e.target !== imagenesLight){
-        contenedorLight.classList.toggle('show')
-        imagenesLight.classList.toggle('showImagen')
-        hamburguer1.style.opacity = '1'
-    }
-
-})
+ contenedorLight.addEventListener('click', (e)=>{
+     if(e.target !== imagenesLight){
+         contenedorLight.classList.toggle('show')
+         imagenesLight.classList.toggle('showImagen')
+         hamburguer1.style.opacity = '1'
+     }
+ 
+ })
 const aparecerImagen = (imagen) =>{
-    imagenesLight.src = imagen
-    contenedorLight.classList.toggle('show')
-    imagenesLight.classList.toggle('showImage')
-    hamburguer1.style.opacity = '0'
+    imagenesLight.src = imagen;
+     contenedorLight.classList.toggle('show')
+     imagenesLight.classList.toggle('showImage')
+     hamburguer1.style.opacity = '0'
 }
